358e5788efd43b43abc52cc9d3b322effceea848
Hillol Pratim Kalita <hillol7@users.noreply.huggingface.co>
2024-06-25 16:35:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
+
 import gradio as gr
+from fastai.vision.all import load_learner, PILImage
+import torch
 
-def greet(name):
-    return "Hello " + name + "!!"
+model_path = 'potato_classifier.pkl'  # Ensure this path is correct
+learn = load_learner(model_path)
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+def classify_image(image):
+    # Predict the class of the image
+    pred, pred_idx, probs = learn.predict(image)
+    return {learn.dls.vocab[i]: float(probs[i]) for i in range(len(probs))}
+
+interface = gr.Interface(
+    fn=classify_image,
+    inputs=gr.inputs.Image(type='pil'),
+    outputs=gr.outputs.Label(num_top_classes=3),
+    title="Potato Plant Disease Classifier",
+    description="Potato leaf: "
+)
+
+if __name__ == "__main__":
+    interface.launch()
